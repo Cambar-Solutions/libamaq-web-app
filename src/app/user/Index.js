2a65d784f9a73460e39c2fdf60c436d0e59f0093
AngelMurga <20233tn150@utez.edu.mx>
2025-06-09 15:19:55 -0600

    Implemented index in the User module

---
+++ src/app/user/Index.js
@@ -0,0 +1,29 @@
+// Atoms
+export { default as Breadcrumb_Buy } from './components/atoms/Breadcrumb_Buy';
+export { default as BtnDelete } from './components/atoms/BtnDelete';
+export { default as BtnSave } from './components/atoms/BtnSave';
+
+// Molecules
+export { default as AppSidebarCustomer } from './components/molecules/app-sidebarCustomer';
+export { default as FilterByDate } from './components/molecules/FilterByDate';
+export { default as NavCustomer } from './components/molecules/NavCustomer';
+export { default as SiteHeaderCustomer } from './components/molecules/site-headerCustomer';
+
+// Forms
+export { default as EditProfile } from './components/forms/EditProfile';
+
+// Organisms
+export { default as CardCarProducts } from './components/organisms/buyPanel/CardCarProducts';
+export { default as CardsDetail } from './components/organisms/buyPanel/CardsDetail';
+export { default as CardProducts } from './components/organisms/CardProducts';
+export { default as DialogAddresses } from './components/organisms/DialogAddresses';
+export { default as SearchBar } from './components/organisms/SearchBar';
+export { default as SheetTerms } from './components/organisms/SheetTerms';
+
+// Pages
+export { default as Account } from './components/pages/Account';
+export { default as BuyPanel } from './components/pages/sidebar/BuyPanel';
+export { default as CarPanel } from './components/pages/sidebar/CarPanel';
+export { default as OrderPanel } from './components/pages/sidebar/OrderPanel';
+export { default as ProfilePanel } from './components/pages/sidebar/ProfilePanel';
+export { default as RentalPanel } from './components/pages/sidebar/RentalPanel';
